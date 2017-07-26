1ce6f4de0a9b30d6e978814736914b8d397d4ace
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2017-07-26 08:32:05 +0200

    vue-migration: agent-widget, Holding component added

--- vue-experiments/agent-widget.js
+++ vue-experiments/agent-widget.js
@@ -6,7 +6,15 @@ Vue.component('agent-widget' , {
 	template: '	<div v-bind:class= "classObject" > 												\
 					<h2> {{title}}</h2> 														\
 					<button v-on:click="init"> start </button> 									\
-					<lock-role :topic_data="lockRole" :robot_name="robot_name"> Loading Lock Role !! </lock-role> 								\
+					<lock-role :topic_data="lockRoleTopic"  									\
+								:robot_name="robot_name"> 										\
+						Loading Lock Role !! 													\
+					</lock-role> 																\
+					<holding :holding_data="holdingTopic" 										\
+							:workpiece_data="productOfHolding" 									\
+							:robot_name="robot_name"> 											\
+						Loading Holding !! 														\
+					</holding> 																	\
 					</div>																		\
 				</div> 																			\
 				',
@@ -21,15 +29,32 @@ Vue.component('agent-widget' , {
   			},
   	    	show: true ,
 	  	    topics: { 
-		  	    	clips: { "lock-role": [] , holding: [] , state: [] , tasks: [] , "locked-resource" : [] , skills: [] },
+		  	    	clips: { "lock-role": [] , holding: [] , state: [] , tasks: [] , "locked-resource" : [] , skills: [] , product: [] },
 	  	    		}
   		}
   	},
   	computed: {
-  		lockRole: function (){
+  		lockRoleTopic: function (){
   			return this.topics.clips["lock-role"][0];
+  		}, 
+
+  		holdingTopic: function (){
+  			return this.topics.clips["holding"][0];
+  		},
+
+ 		productOfHolding: function (){
+			// console.log( order["product-id"][0].constructor )
+  			for (var i in this.topics.clips.product){
+  				var product = this.topics.clips.product[i];
+  				// console.log( order["product-id"][0] )
+  				// console.log(product["id"][0]);
+  				if (product["id"][0] == this.topics.clips["holding"][0].fields[0] )
+				{
+					return product
+				}
+				}
   		}
-  	}
+  	},
 });
 
 
@@ -56,8 +81,7 @@ Vue.component('lock-role' , {
 	  			container_header_element : true,
 	  			master : (this.topic_data.fields[0] == "MASTER"),
 	  			slave : (this.topic_data.fields[0] != "MASTER") 
-  			}
-  				
+  			}	
   		},
 
   		generatedID: function(){
@@ -66,6 +90,42 @@ Vue.component('lock-role' , {
 
   		isMaster: function(){
   			return this.topic_data.fields[0] == "MASTER"; 
+  		}
+
+  	}
+});
+
+
+Vue.component('holding' , { 
+	props: [ 'holding_data', 'workpiece_data' , 'robot_name' ],
+	template: '	<div :id="generatedID"																\
+					 :class= "classObject" >														\
+					<p> Holding: {{holding}}</p>													\
+					<workpiece :topic_data="workpiece_data"> Loading Workpiece !! </workpiece> 		\
+				</div> 																				\
+				',
+
+	data: function () {
+		return{
+			id_prefix: "holding__",
+  	    	show: true 
+  		}
+  	},
+
+  	computed: {
+  		classObject: function (){
+  			return {
+	  			wedgit : true,
+	  			container_header_element : true
+  			}	
+  		},
+
+  		generatedID: function(){
+  			return this.id_prefix + this.robot_name;
+  		},
+
+  		holding: function(){
+  			return this.holding_data.fields[0];
   		}
 
   	}
