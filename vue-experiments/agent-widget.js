3eb5d4150e2b5bfb0d77ed023c4ef3a3ddf3f6bb
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2017-07-26 07:30:18 +0200

    vue-migration: agent-widget, Lock-Role added

--- vue-experiments/agent-widget.js
+++ vue-experiments/agent-widget.js
@@ -6,6 +6,7 @@ Vue.component('agent-widget' , {
 	template: '	<div v-bind:class= "classObject" > 												\
 					<h2> {{title}}</h2> 														\
 					<button v-on:click="init"> start </button> 									\
+					<lock-role :topic_data="lockRole" :robot_name="robot_name"> Loading Lock Role !! </lock-role> 								\
 					</div>																		\
 				</div> 																			\
 				',
@@ -13,18 +14,66 @@ Vue.component('agent-widget' , {
 	data: function () {
 		return{
 			id : "agent_widget",
-
-			title: "Agent",
-
+			title: "Agent Info",
+			robot_name: "Rx",
 			classObject: {
-  				col_element : true
+  				container : true
   			},
-
   	    	show: true ,
-
 	  	    topics: { 
 		  	    	clips: { "lock-role": [] , holding: [] , state: [] , tasks: [] , "locked-resource" : [] , skills: [] },
 	  	    		}
   		}
+  	},
+  	computed: {
+  		lockRole: function (){
+  			return this.topics.clips["lock-role"][0];
+  		}
   	}
 });
+
+
+Vue.component('lock-role' , { 
+	props: [ 'topic_data' , 'robot_name' ],
+	template: '	<div :id="generatedID"												\
+					 :class= "classObject" >										\
+						<img> </img> 												\
+						<span v-if="!isMaster"> {{ topic_data.fields[0] }}</span>	\
+				</div> 																\
+				',
+
+	data: function () {
+		return{
+			id_prefix: "lock_role__",
+  	    	show: true ,
+  		}
+  	},
+
+  	computed: {
+  		classObject: function (){
+  			return {
+	  			wedgit : true,
+	  			container_header_element : true,
+	  			master : (this.topic_data.fields[0] == "MASTER"),
+	  			slave : (this.topic_data.fields[0] != "MASTER") 
+  			}
+  				
+  		},
+
+  		generatedID: function(){
+  			return this.id_prefix + this.robot_name;
+  		},
+
+  		isMaster: function(){
+  			return this.topic_data.fields[0] == "MASTER"; 
+  		}
+
+  	}
+});
+
+
+
+
+
+//TODO IN CSS RESTUCTURE ROUND
+// let the CSS not use ids..I dont like ids
