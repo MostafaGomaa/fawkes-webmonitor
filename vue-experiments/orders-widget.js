99adb1b1b6f89b2891ebf63e9e42ad1c80f57b3e
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2017-07-26 05:33:38 +0200

    vue-migration: order-widget completed
    - order-widget creates and order component for each order topic
    - order component creates the workpiece and info components

--- vue-experiments/orders-widget.js
+++ vue-experiments/orders-widget.js
@@ -1,73 +1,8 @@
-// Vue.component('order' , { 
-// 	template: '\
-// 		<div v-bind:class="classObject">\
-		
-		  
-// 		 </div>\
-// 	',
-	
-//   data: function () {
-//   	return {
-//   	   classObject:{
-//   	   	order: true,
 
-
-//   	   } 
-
-
-//   	}
-//   },
-
-//   methods: {
-
-//   	subscribe: function (){  
-
-// 		var ros_topic = new ROSLIB.Topic({
-// 		      ros : ros,
-// 		      name : 'some_topic',
-// 		      messageType : 'mm',
-// 		      throttle_rate:window.throttle_rate ,
-// 		    });
-
-// 	  	ros_topic .name = this.input_text;	 	
-// 	  	ros_topic .subscribe(this.updateValues);
-// 	},
-	
-// 	updateValues: function(message) {
-
-// 		// for (var key in message)//present the content in the div 
-// 		// {
-// 		// 	var div_content = key + " : ";
-// 		// 	if(message[key].constructor === Array)
-// 		// 	{
-// 		// 		for( var fact in message[key] )
-// 		// 		{
-// 		// 			div_content+= JSON.stringify(message[key][fact], null,e 5) + "<br>"; //TODO: maybe i dont need to stringfiy 
-// 		// 		}
-// 		// 	}
-// 		// 	else
-// 		// 	{
-// 		// 		div_content+= message[key] + "<br>"; 
-// 		// 	}
-
-// 		// 	div_content+=  "<br>"; 
-// 		// 	$(div_id).append(div_content);
-// 		// }
-
-// 		console.log ( message );	
-// 	  	console.log ( this.input_text );
-// 	}
-	
-//   }
 
 
 Vue.component('workpiece' , { 
-			// \
-			// 	<div v-for = "item in workpiece_date.rings"									\
-			// 		 :style="{ backgroundColor : item }"									\
-			// 		 :class="classObject(workpieceTypes.ring)" > 							\
-			// 		</div> 															
-	props: ['workpiece_data'],	
+	props: ['topic_data'],
 	template: '																				\
 			<div :style="styleObject" :class="classObject(workpieceTypes.product)">			\
 				<div :style="{ backgroundColor : getCapColor }"								\
@@ -99,7 +34,7 @@ Vue.component('workpiece' , {
 				ring: 2,
 				product:3
 			}
-  		}	
+  		}
   	},
 
   	methods : {
@@ -113,7 +48,7 @@ Vue.component('workpiece' , {
   		},
 
   		getRingColor: function (ring_index){
-  			return this.workpiece_data["rings"][ring_index];
+  			return this.topic_data["rings"][ring_index];
 
   		}
   	},
@@ -122,17 +57,17 @@ Vue.component('workpiece' , {
   	computed: {
 
   		getBaseColor: function (){
-  			return this.workpiece_data["base"][0];
+  			return this.topic_data["base"][0];
 
   		},
 
   		getCapColor: function (){
-  			return this.workpiece_data["cap"][0];
+  			return this.topic_data["cap"][0];
 
   		},
 
   		getRingCount: function (){
-  			return this.workpiece_data["rings"].length;
+  			return this.topic_data["rings"].length;
 
   		}
   	}
@@ -140,19 +75,64 @@ Vue.component('workpiece' , {
 
 });
 
+Vue.component('order-info',  {
+	props: ['topic_data'],
+	template: '																											\
+			<div :class="classObject">																					\
+				<span> Gate: {{ topic_data["delivery-gate"][0] }} </span>												\
+				<br>																									\
+				<span> {{ topic_data["begin"][0] + ":" + topic_data["end"][0] }} </span>								\
+				<br>																									\
+				<span> {{ topic_data["quantity-delivered"][0] + "/" + topic_data["quantity-requested"][0] }} </span>	\
+				<br>																									\
+			</div>',
+
+	data: function () {
+		return{
+			classObject: { order_info: true }
+		}
+	}
+});
+
+
+Vue.component('order' , { 
+	props: [ 'order_data' , 'product_data' ],
+	template: '	<div v-bind:class= "classObject" >															\
+						<workpiece :topic_data="product_data" > workpiece Not loaded !! </workpiece>		\
+						<br>																				\
+						<order-info :topic_data="order_data" > OrderInfo Not loaded !! </order-info>		\
+					</div>																					\
+				</div> 																						\
+				',
+
+	data: function () {
+		return{
+			id : "",
+
+			classObject: {
+  				order : true
+  			},
+
+  	    	show: true
+  		}
+  	}
+});
+
 
 Vue.component('orders-widget' , { 
-	
+
 	extends : widget,
-	
-	template: '	<div v-bind:class= "classObject" > 							\
-					<h2> {{title}}</h2> 									\
-					<button v-on:click="init"> starr </button> 				\
-					<div v-for= "item in topics.clips.order">				\
-						<workpiece :workpiece_data="getProductForOrder(item)"  > </workpiece>\
-						<br>												\
-					</div>													\
-				</div> 														\
+
+	template: '	<div v-bind:class= "classObject" > 												\
+					<h2> {{title}}</h2> 														\
+					<button v-on:click="init"> starr </button> 									\
+					<div v-for= "item in topics.clips.order">									\
+						<order 	:product_data="getProductForOrder(item)" 						\
+								:order_data=item > 												\
+								Order Not Loaded !! 											\
+						</order>																\
+					</div>																		\
+				</div> 																			\
 				',
 
 	data: function () {
@@ -164,13 +144,13 @@ Vue.component('orders-widget' , {
 			classObject: {
   				col_element : true
   			},
-		
+
   	    	show: true ,
 
 	  	    topics: { 
 		  	    	clips: { order: [] , product: [] },
 	  	    		}
-  		}	
+  		}
   	}
 
   ,
@@ -190,7 +170,7 @@ Vue.component('orders-widget' , {
   				var product = this.topics.clips.product[i];
   				// console.log( order["product-id"][0] )
   				// console.log(product["id"][0]);
-  	
+  
   				if (product["id"][0] == order["product-id"][0] )
 				{
 					return product
@@ -202,3 +182,66 @@ Vue.component('orders-widget' , {
 
 });
 
+
+//OLD CODE
+// Vue.component('order' , { 
+// 	template: '\
+// 		<div v-bind:class="classObject">\
+
+		  
+// 		 </div>\
+// 	',
+
+//   data: function () {
+//   	return {
+//   	   classObject:{
+//   	   	order: true,
+
+
+//   	   } 
+
+
+//   	}
+//   },
+
+//   methods: {
+
+//   	subscribe: function (){  
+
+// 		var ros_topic = new ROSLIB.Topic({
+// 		      ros : ros,
+// 		      name : 'some_topic',
+// 		      messageType : 'mm',
+// 		      throttle_rate:window.throttle_rate ,
+// 		    });
+
+// 	  	ros_topic .name = this.input_text;
+// 	  	ros_topic .subscribe(this.updateValues);
+// 	},
+
+// 	updateValues: function(message) {
+
+// 		// for (var key in message)//present the content in the div 
+// 		// {
+// 		// 	var div_content = key + " : ";
+// 		// 	if(message[key].constructor === Array)
+// 		// 	{
+// 		// 		for( var fact in message[key] )
+// 		// 		{
+// 		// 			div_content+= JSON.stringify(message[key][fact], null,e 5) + "<br>"; //TODO: maybe i dont need to stringfiy 
+// 		// 		}
+// 		// 	}
+// 		// 	else
+// 		// 	{
+// 		// 		div_content+= message[key] + "<br>"; 
+// 		// 	}
+
+// 		// 	div_content+=  "<br>"; 
+// 		// 	$(div_id).append(div_content);
+// 		// }
+
+// 		console.log ( message );
+// 	  	console.log ( this.input_text );
+// 	}
+
+//   }
